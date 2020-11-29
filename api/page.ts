3f67f39d0cfbe3c33ab946ed3ac65c50d68d3e14
takker99 <37929109+takker99@users.noreply.github.com>
2020-11-29 11:22:24 +0900

    :sparkles: Enable to get a title and OGP data from URL
    error処理は真面目にやっていません

---
+++ api/page.ts
@@ -0,0 +1,64 @@
+import { ServerRequest } from "https://deno.land/std/http/server.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+interface URLInfoResponse {
+  url: string;
+  title?: string;
+  ogps?: {
+    property?: string;
+    content?: string;
+  }[];
+}
+
+export default async (req: ServerRequest) => {
+  // 引数を受け取る
+  const base = `${req.headers.get("x-forwarded-proto")}://${req.headers.get(
+    "x-forwarded-host"
+  )}`;
+  const url = new URL(req.url, base);
+  const params = url.searchParams;
+  const targetURL = params.get("url");
+
+  if (!targetURL) {
+    respondJSON({ error: "No URL was found." }, req);
+    return;
+  }
+  // jsonにして返す
+  let result: URLInfoResponse = { url: targetURL };
+
+  // html textをfetchする
+  console.log(`Fetching HTML from ${targetURL}...`);
+  try {
+    const res = await fetch(targetURL);
+    console.log("Success! response: %o", res);
+    const html = await res.text();
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    // titleを取得する
+    result.title = doc?.getElementsByTagName("title")?.[0]?.textContent;
+
+    // OGPを取得する
+    result.ogps = doc?.getElementsByTagName("meta").map((meta) => {
+      return {
+        property:
+          meta.getAttribute("name") ??
+          meta.getAttribute("property") ??
+          undefined,
+        content: meta.getAttribute("content") ?? undefined,
+      };
+    });
+
+    console.log("Return this json: %o", result);
+    respondJSON(result, req);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+function respondJSON<T extends {}>(json: T, req: ServerRequest) {
+  const headers = new Headers();
+  headers.set("Content-Type", "application/json; charset=utf8");
+  req.respond({
+    headers,
+    body: JSON.stringify(json, null, 2),
+  });
+}
